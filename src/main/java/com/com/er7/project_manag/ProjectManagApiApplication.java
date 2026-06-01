6066748b1a24847220c9f303f204e434d34d441c
CarlosEReis <carlos.er7@gmail.com>
2026-06-01 20:02:29 -0300

    build: arquivos de inicializacao

---
+++ src/main/java/com/com/er7/project_manag/ProjectManagApiApplication.java
@@ -0,0 +1,13 @@
+package com.com.er7.project_manag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/com/er7/project_manag/ProjectManagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.er7.project_manag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
