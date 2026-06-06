602ead9d71d477265b96cb373d56b687d1c53b1c
CarlosEReis <carlos.er7@gmail.com>
2026-06-06 14:59:59 -0300

    build: add carga de dados para as listas

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO lista (id, public_id, nome, descricao, cor, ordem, deleted) VALUES
+(1, '019e9db8-d07d-77d2-aa79-b2742e7e7f0d', 'Backlog', 'Tickets em Backlog representando tarefas pendentes.', '#dc8add', 1, false),
+(2, '019e9db8-d07f-768d-af0b-40e63a290dbf', 'A Fazer', 'Tarefas filtradas do backlog para serem iniciadas.', '#ffbe6f', 2, false),
+(3, '019e9db8-d07f-7763-aa48-19a5136b41c7', 'Em Progresso', 'Tickets em andamento, indicando que estão sendo trabalhados.', '#22c55e', 3, false),
+(4, '019e9db8-d07f-77ad-8a15-8dbb9c8cce90', 'Atualizar Homol', 'Tarefas relacionadas à atualização do ambiente de homologação.', '#38bdf8', 4, false),
+(5, '019e9db8-d07f-77ee-8ef3-3011198303d1', 'Validar Homl', 'Tarefas para validar as mudanças no ambiente de homologação.', '#f9f06b', 5, false),
+(6, '019e9db8-d07f-7824-9233-1960195bb870', 'Atualizar Prod', 'Tarefas relacionadas à atualização do ambiente de produção.', '#f59e0b', 6, false),
+(7, '019e9db8-d07f-7843-97fc-1a813b18a990', 'Finalizado', 'Tickets finalizados, indicando que as tarefas foram concluídas.', '#dcfce7', 7, false),
+(8, '019e9db8-d07f-786b-8a60-91256c5ccb7c', 'Pausado', 'Tickets pausados, indicando que as tarefas foram interrompidas.', '#dcfce7', 8, false),
+(9, '019e9db8-d07f-78a1-b597-742cf2c56339', 'Aprovação', 'Tickets em aprovação, indicando que aguardam aprovação.', '#f59e0b', 9, false),
+(10, '019e9db8-d07f-78d7-80ec-85445128f358', 'Em Revisão', 'Tarefas que estão sendo revisadas para garantir a qualidade.', '#a78bfa', 10, false),
+(11, '019e9db8-d07f-790f-aa05-9711822ebde3', 'Aguardando Feedback', 'Tarefas que estão aguardando feedback de stakeholders ou clientes.', '#f87171', 11, false),
+(12, '019e9db8-d07f-7947-928b-5d379c4281e6', 'Em Teste', 'Tarefas que estão sendo testadas para garantir que atendem aos requisitos.', '#34d399', 12, false);
